896e26b0ab7b64010936401b105c46ce7afcc6cf
danielbarrosdeoliveira <danielbarrosdeoliveira@outlook.com>
2022-06-03 18:06:09 -0300

    chore: adjustment color info text

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -105,7 +105,7 @@ class _HomeState extends State<Home> {
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 18,
-                    color: Colors.grey[500],
+                    color: Colors.grey[700],
                     height: 1.5,
                   ),
                 ),
